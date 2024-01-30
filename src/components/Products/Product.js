391de67578c205f846f4f4ba13be9acbb17232eb
NaviBoy23 <naveenbalaji.gs@gmail.com>
2024-01-30 13:04:01 +0530

    first commit

---
+++ src/components/Products/Product.js
@@ -0,0 +1,14 @@
+import "./Product.css";
+const Product = ({ image, price, title }) => (
+  <div className="product-card">
+    <div className="product-image">
+      <img src={image} />
+    </div>
+    <div className="product-details">
+      <h3>{title}</h3>
+      <span>{price}</span>
+    </div>
+  </div>
+);
+
+export default Product;
